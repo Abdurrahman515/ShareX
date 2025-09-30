91d3f0d846429dc3d45f39e3f435912db1e26124
Abdurrahman515 <karbanbaroudia@gmail.com>
2025-09-30 10:44:22 +0300

    error handling problem for arabic solved

--- frontend/src/components/other/LoginCard.jsx
+++ frontend/src/components/other/LoginCard.jsx
@@ -35,7 +35,7 @@ export default function LoginCard() {
     };
     setLoading(true);
     try {
-      const res = await fetch("/api/users/login", {
+      const res = await fetch(`/api/users/login?lang=${lang}`, {
         method: "POST",
         headers: {
           "Content-Type" : "application/json"
